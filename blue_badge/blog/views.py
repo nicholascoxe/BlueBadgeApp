1c4b0c6d6795d48487040b0964c900535d5b803b
Nicholas Coxe <autumncoxe@Nicholass-MacBook-Pro.local>
2019-03-07 11:05:30 -0500

    almost had to nuke

--- blue_badge/blog/views.py
+++ blue_badge/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 
@@ -7,6 +8,13 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 
 def about(request):
