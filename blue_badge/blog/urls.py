4abcc994ce79b515cc645189d9af27cca475e13e
Logan Chaves <loganchaves@icloud.com>
2019-03-12 08:23:31 -0700

    added two pages

--- blue_badge/blog/urls.py
+++ blue_badge/blog/urls.py
@@ -10,6 +10,7 @@ from .views import (
 from . import views
 
 urlpatterns = [
+   
     path('', PostListView.as_view(), name='blog-home'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
@@ -17,4 +18,6 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
+    path('contact/', views.contact, name='blog-contact' ),
+    path('store/', views.store, name= 'blog-store')
 ]
