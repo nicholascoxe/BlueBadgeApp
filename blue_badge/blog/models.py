ffcb2578bb40cb532a766bb8aa8de9ac03d4294c
Logan Chaves <loganchaves@icloud.com>
2019-03-19 07:31:09 -0700

    location field

--- blue_badge/blog/models.py
+++ blue_badge/blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now())
     author = models.ForeignKey(User, on_delete=models.CASCADE)
--- blue_badge/blog/views.py
+++ blue_badge/blog/views.py
@@ -42,7 +42,7 @@ class PostDetailView(DetailView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['location', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -51,7 +51,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['location', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
